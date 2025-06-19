d459c0dc4d29075b01147ccf702b78d7daf24962
ManoellaKouam <michelle.kouam-wogaing@stud.thga>
2025-06-19 21:18:35 +0200

    Implement flowchart algorithm with main and reflection

---
+++ solutions/flowchart_impl.c
@@ -0,0 +1,7 @@
+// solutions/flowchart_impl.h
+#ifndef FLOWCHART_IMPL_H
+#define FLOWCHART_IMPL_H
+
+int flowchart_impl(int x){};
+
+#endif
---
+++ solutions/main.c
@@ -0,0 +1,8 @@
+// solutions/main.c
+#include "flowchart_impl.h"
+
+int main() {
+    int x = 10; // tu peux changer la valeur pour tester
+    flowchart_impl(x);
+    return 0;
+}
